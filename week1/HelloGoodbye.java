95ae3e28179f4ed9395bbf2f69aa37f363c13624
Zepyoor Khechadoorian <zk77@cornell.edu>
2024-06-21 08:41:11 -0400

    Committed simple io scripts as part of week 1 assignment.

---
+++ week1/HelloGoodbye.java
@@ -0,0 +1,9 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        StdOut.printf("Hello %s and %s.%n", args[0], args[1]);
+        StdOut.printf("Goodbye %s and %s.%n", args[1], args[0]);
+    }
+}
